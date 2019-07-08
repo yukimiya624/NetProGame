58e31e6d7ed83763b00c93e4c8ade7565929d551
Gamu2059 <yamagami0711syo@gmail.com>
2019-07-08 16:32:24 +0900

    Update : プロジェクトの整理

---
+++ Assets/Scripts/MatchLobby/MatchRequestProcess.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchRequestProcess : MonoBehaviour
+{
+    [SerializeField]
+    private Dropdown m_Dropdown;
+
+    [SerializeField]
+    private Button m_RequestMatchButton;
+
+    private void Start()
+    {
+        m_Dropdown.ClearOptions();
+        var addresses = NetproNetworkManager.Instance.GetSelfIpAddresses();
+        var list = new List<Dropdown.OptionData>();
+        foreach (var addr in addresses)
+        {
+            var data = new Dropdown.OptionData();
+            data.text = addr.ToString();
+            list.Add(data);
+        }
+
+        m_Dropdown.AddOptions(list);
+
+        m_RequestMatchButton.onClick.AddListener(OnClickRequestMatch);
+    }
+
+    private void OnClickRequestMatch()
+    {
+        var idx = m_Dropdown.value;
+        var data = m_Dropdown.options[idx];
+        GameManager.Instance.RequestMatch(data.text);
+    }
+}
---
+++ Assets/Scripts/Utility/Utility/EventUtility.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine.Events;
+
+public class EventUtility
+{
+
+	public static void SafeInvokeAction( Action action )
+	{
+		if( action != null )
+			action.Invoke();
+	}
+
+	public static void SafeInvokeAction<T>( Action<T> action, T p1 )
+	{
+		if( action != null )
+			action.Invoke( p1 );
+	}
+
+	public static void SafeInvokeAction<T, U>( Action<T, U> action, T p1, U p2 )
+	{
+		if( action != null )
+			action.Invoke( p1, p2 );
+	}
+
+	public static void SafeInvokeAction<T, U, V>( Action<T, U, V> action, T p1, U p2, V p3 )
+	{
+		if( action != null )
+			action.Invoke( p1, p2, p3 );
+	}
+
+	public static void SafeInvokeAction<T, U, V, W>( Action<T, U, V, W> action, T p1, U p2, V p3, W p4 )
+	{
+		if( action != null )
+			action.Invoke( p1, p2, p3, p4 );
+	}
+
+	public static void SafeInvokeUnityEvent( UnityEvent uEvent )
+	{
+		if( uEvent != null )
+			uEvent.Invoke();
+	}
+
+	public static void SafeInvokeUnityEvent<T>( UnityEvent<T> uEvent, T p1 )
+	{
+		if( uEvent != null )
+			uEvent.Invoke( p1 );
+	}
+
+	public static void SafeInvokeUnityEvent<T, U>( UnityEvent<T, U> uEvent, T p1, U p2 )
+	{
+		if( uEvent != null )
+			uEvent.Invoke( p1, p2 );
+	}
+
+	public static void SafeInvokeUnityEvent<T, U, V>( UnityEvent<T, U, V> uEvent, T p1, U p2, V p3 )
+	{
+		if( uEvent != null )
+			uEvent.Invoke( p1, p2, p3 );
+	}
+
+	public static void SafeInvokeUnityEvent<T, U, V, W>( UnityEvent<T, U, V, W> uEvent, T p1, U p2, V p3, W p4 )
+	{
+		if( uEvent != null )
+			uEvent.Invoke( p1, p2, p3, p4 );
+	}
+}
